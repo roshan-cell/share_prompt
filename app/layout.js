a5c3681379f2c3b4d969aeecafa481a461d06bfb
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-07-14 11:09:38 +0530

    provider

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,28 @@
 import '@styles/global.css'
 
+import Nav from '@components/Nav' ;
+import Provider from '@components/Provider' ;
+
 export const metadata = {
     title : 'Promttopia' ,
     description : 'Discover & Share AI prompts'
 }
 
-
 const RootLayout = ({children}) => {
   return (
     <html lang="en">
         <body>
+            <Provider>
             <div className="main">
                 <div className="gradient" />
             </div>
 
             <main className="app">
+                <Nav/>
                 {children}
             </main>
+
+            </Provider>
         </body>
     </html>
   )
